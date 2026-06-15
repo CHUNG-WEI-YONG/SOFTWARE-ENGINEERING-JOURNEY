c5bee238339cbd3117275b1fea9839d0700b01b0
CHUNG WEI YONG <chungwyong@gmail.com>
2026-06-15 19:09:58 +0800

    Logic System Added

---
+++ full-stack-project/cpp_project/WhatsApp_like_app/build/gateserver/CMakeFiles/GateServer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ASUSTUF/Desktop/SE_JOURNEY/full-stack-project/cpp_project/WhatsApp_like_app/gateserver/source_file/Cserver.cpp" "gateserver/CMakeFiles/GateServer.dir/source_file/Cserver.cpp.obj" "gcc" "gateserver/CMakeFiles/GateServer.dir/source_file/Cserver.cpp.obj.d"
+  "C:/Users/ASUSTUF/Desktop/SE_JOURNEY/full-stack-project/cpp_project/WhatsApp_like_app/gateserver/source_file/GateServer.cpp" "gateserver/CMakeFiles/GateServer.dir/source_file/GateServer.cpp.obj" "gcc" "gateserver/CMakeFiles/GateServer.dir/source_file/GateServer.cpp.obj.d"
+  "C:/Users/ASUSTUF/Desktop/SE_JOURNEY/full-stack-project/cpp_project/WhatsApp_like_app/gateserver/source_file/HttpConnection.cpp" "gateserver/CMakeFiles/GateServer.dir/source_file/HttpConnection.cpp.obj" "gcc" "gateserver/CMakeFiles/GateServer.dir/source_file/HttpConnection.cpp.obj.d"
+  "C:/Users/ASUSTUF/Desktop/SE_JOURNEY/full-stack-project/cpp_project/WhatsApp_like_app/gateserver/source_file/LogicSystem.cpp" "gateserver/CMakeFiles/GateServer.dir/source_file/LogicSystem.cpp.obj" "gcc" "gateserver/CMakeFiles/GateServer.dir/source_file/LogicSystem.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ full-stack-project/cpp_project/WhatsApp_like_app/build/gateserver/CMakeFiles/GateServer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GateServer.dir/source_file/Cserver.cpp.obj"
+  "CMakeFiles/GateServer.dir/source_file/Cserver.cpp.obj.d"
+  "CMakeFiles/GateServer.dir/source_file/GateServer.cpp.obj"
+  "CMakeFiles/GateServer.dir/source_file/GateServer.cpp.obj.d"
+  "CMakeFiles/GateServer.dir/source_file/HttpConnection.cpp.obj"
+  "CMakeFiles/GateServer.dir/source_file/HttpConnection.cpp.obj.d"
+  "CMakeFiles/GateServer.dir/source_file/LogicSystem.cpp.obj"
+  "CMakeFiles/GateServer.dir/source_file/LogicSystem.cpp.obj.d"
+  "GateServer.exe"
+  "GateServer.exe.manifest"
+  "GateServer.pdb"
+  "libGateServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GateServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
